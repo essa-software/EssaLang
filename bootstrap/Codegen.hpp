706810c00d4adff30ae587ae6b5c459f94a4deac
Maciej <sppmacd@pm.me>
2022-10-10 09:47:19 +0200

    Codegen: Start implementing basic code generation :^)

---
+++ bootstrap/Codegen.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Typechecker.hpp"
+#include <EssaUtil/Stream.hpp>
+
+namespace ESL::Codegen {
+
+class CodeGenerator {
+public:
+    CodeGenerator(Util::Writer& writer, Typechecker::CheckedProgram const& program)
+        : m_writer(writer)
+        , m_program(program) { }
+
+    Util::OsErrorOr<void> codegen();
+
+private:
+    Util::OsErrorOr<void> codegen_prelude();
+    Util::OsErrorOr<void> codegen_epilogue();
+    Util::OsErrorOr<void> codegen_function(Typechecker::CheckedFunction const&);
+    Util::OsErrorOr<void> codegen_type(Typechecker::Type const&);
+    Util::OsErrorOr<void> codegen_block(Typechecker::CheckedBlock const&);
+    Util::OsErrorOr<void> codegen_expression(Typechecker::CheckedExpression const&);
+    Util::OsErrorOr<void> codegen_binary_expression(Typechecker::CheckedExpression::BinaryExpression const& expression);
+
+    Util::Writer& m_writer;
+    Typechecker::CheckedProgram const& m_program;
+};
+
+}
--- bootstrap/main.cpp
+++ bootstrap/main.cpp
@@ -1,49 +1,60 @@
+#include "Codegen.hpp"
 #include "Lexer.hpp"
 #include "Parser.hpp"
 #include "Typechecker.hpp"
 #include <EssaUtil/Stream.hpp>
+#include <EssaUtil/Stream/StandardStreams.hpp>
 
 // TODO: TYPECHECKER
 // TODO: CODEGEN
 
-Util::OsErrorOr<void> run_esl(std::string const& file_name) {
+Util::OsErrorOr<bool> run_esl(std::string const& file_name) {
     auto stream = TRY(Util::ReadableFileStream::open(file_name));
 
     ESL::Lexer lexer { stream };
     auto tokens = TRY(lexer.lex());
 
-    for (auto const& token : tokens) {
-        fmt::print("{} {}\n", (int)token.type(), token.value());
-    }
+    // for (auto const& token : tokens) {
+    //     fmt::print("{} {}\n", (int)token.type(), token.value());
+    // }
 
     ESL::Parser::Parser parser { tokens };
     auto parsed_file = parser.parse_file();
     if (parsed_file.is_error()) {
         auto error = parsed_file.release_error();
         fmt::print("Parse error: {} at {}:{}\n", error.message, error.location.start.line + 1, error.location.start.column + 1);
-        return {};
+        return false;
     }
-    parsed_file.value().print();
+    // parsed_file.value().print();
 
     ESL::Typechecker::Typechecker typechecker { parsed_file.release_value() };
     auto const& program = typechecker.typecheck();
     for (auto const& error : typechecker.errors()) {
         fmt::print("Typechecker error: {} at {}:{}\n", error.message, error.range.start.line + 1, error.range.start.column + 1);
     }
-    program.print();
-    return {};
+    if (!typechecker.errors().empty()) {
+        return false;
+    }
+    // program.print();
+
+    Util::Writer writer { Util::std_out() };
+    ESL::Codegen::CodeGenerator code_generator { writer, program };
+    TRY(code_generator.codegen());
+    return true;
 }
 
 int main(int argc, char** argv) {
     if (argc < 2) {
         fmt::print("Usage: esl <file name>\n");
-        return 1;
+        return 3;
     }
     auto err = run_esl(argv[1]);
     if (err.is_error()) {
         auto error = err.release_error();
         fmt::print("Failed to open esl: {}: {}\n", error.function, strerror(error.error));
-        return 1;
+        return 2;
     }
+    if (!err.release_value())
+        return 1;
     return 0;
 }
