dfea614bba7b8395ed0fe2e07a56032bacf47706
Maciej <sppmacd@pm.me>
2023-07-08 14:38:21 +0200

    Tests: Print test path relative to test dir

--- run-tests.py
+++ run-tests.py
@@ -27,35 +27,38 @@ failed = 0
 passed = 0
 
 
-def fail(test, cause):
+def fail(test_path, cause):
+    test_name = test_path.relative_to(TESTS_DIR)
     global failed
     failed += 1
-    print(f"\033[41m FAIL \033[0;34m {test}\033[m: {cause}")
+    print(f"\033[41m FAIL \033[0;34m {test_name}\033[m: {cause}")
 
 
-def pass_(test, compiler_time):
+def pass_(test_path, compiler_time):
+    test_name = test_path.relative_to(TESTS_DIR)
     global passed
     passed += 1
     print(
-        f"\033[42m PASS \033[0;34m {test} \033[30m({compiler_time/1e9:.2f}s)\033[m")
+        f"\033[42m PASS \033[0;34m {test_name} \033[30m({compiler_time/1e9:.2f}s)\033[m")
 
 
 total_start_time = time.perf_counter_ns()
 
-for p in Path(TESTS_DIR).rglob("*.esl"):
+for test_path in Path(TESTS_DIR).rglob("*.esl"):
     try:
         # print(p)
         expected_out = load_expectation(
-            os.path.join(TESTS_DIR, str(p) + ".out"))
+            os.path.join(TESTS_DIR, str(test_path) + ".out"))
         expected_err = load_expectation(
-            os.path.join(TESTS_DIR, str(p) + ".err"))
+            os.path.join(TESTS_DIR, str(test_path) + ".err"))
 
         if len(expected_out) == 0 and len(expected_err) == 0:
             continue
 
         start_time = time.perf_counter_ns()
 
-        proc = sp.Popen([COMPILER_PATH, str(p)], stdout=sp.PIPE, stderr=sp.PIPE)
+        proc = sp.Popen([COMPILER_PATH, str(test_path)],
+                        stdout=sp.PIPE, stderr=sp.PIPE)
         out, err = proc.communicate()
         ret = proc.returncode
         compiler_time = time.perf_counter_ns() - start_time
@@ -64,12 +67,12 @@ for p in Path(TESTS_DIR).rglob("*.esl"):
         if ret != 0:
             if err != expected_err:
                 if len(out) > 0:
-                    fail(p, "expected success but got compiler error")
+                    fail(test_path, "expected success but got compiler error")
                 else:
-                    fail(p, "got different compile error message than expected")
+                    fail(test_path, "got different compile error message than expected")
                 continue
         elif len(err) > 0:
-            fail(p, "expected compile error but got success")
+            fail(test_path, "expected compile error but got success")
             continue
 
         proc = sp.Popen([os.path.join(ROOT, "build", "build", "out")],
@@ -80,17 +83,17 @@ for p in Path(TESTS_DIR).rglob("*.esl"):
         if ret != 0:
             if err != expected_err:
                 if len(out) > 0:
-                    fail(p, "expected success but got runtime error")
+                    fail(test_path, "expected success but got runtime error")
                 else:
-                    fail(p, "got different runtime error message than expected")
+                    fail(test_path, "got different runtime error message than expected")
                 continue
         elif len(err) > 0:
-            fail(p, "expected runtime error but got success")
+            fail(test_path, "expected runtime error but got success")
             continue
 
-        pass_(p, compiler_time)
+        pass_(test_path, compiler_time)
     except Exception as e:
-        fail(p, str(e))
+        fail(test_path, str(e))
 
 print(
     f"\n\033[32mPassed:\033[m {passed}, \033[31mFailed:\033[m {failed}, Took {(time.perf_counter_ns() - total_start_time)/1e9:.2f}s")
